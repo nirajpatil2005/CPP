3dacb851b514b78cd773351fe2f3a7f95cc50b2c
nirajpatil2005 <nirajzpatil123@gmail.com>
2024-02-13 09:25:15 +0530

    string

--- Recursion/RECURSION III/ARRAY/SkipACharInString.cpp
+++ Recursion/RECURSION III/ARRAY/SkipACharInString.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
-
+void removechar(string ans, string original)
+{
+    if (original.length() == 0)
+    {
+        cout << ans;
+        return;
+    }
+    char ch = original[0];
+    if (ch == 'A')
+        removechar(ans, original.substr(1));
+    else
+        removechar(ans + ch, original.substr(1));
+}
 int main()
 {
     string str = "NIRAJ PATIL";
     string s = "";
-    for (int i = 0; i < str.length(); i++)
-    {
-        if (str[i] != 'A')s=s+str[i];
-    }
-    cout<<s<<endl;
+    // for (int i = 0; i < str.length(); i++)
+    // {
+    //     if (str[i] != 'A')s=s+str[i];
+    // }
+    // cout<<s<<endl;
+    removechar("", str);
     return 0;
 }
