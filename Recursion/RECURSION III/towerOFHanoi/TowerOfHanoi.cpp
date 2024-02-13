1b8f56e479c7d035fb5ee7bd51e3aa5609ca0606
nirajpatil2005 <nirajzpatil123@gmail.com>
2024-02-13 08:14:18 +0530

    tower of hanoi

---
+++ Recursion/RECURSION III/towerOFHanoi/TowerOfHanoi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+//n->no of disk s->source h->helper d->destination
+void hanoi(int n,char s,char h,char d)
+{
+    if(n==0)return;
+    hanoi(n-1,s,d,h);
+    cout<<s<<"-> "<<d<<endl;
+    hanoi(n-1,h,s,d);
+}
+int main()
+{
+    int n=4;
+    hanoi(n,'A','B','C');
+
+    return 0;
+}
