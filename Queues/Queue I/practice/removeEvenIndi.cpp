7af0758e7c10b1dc8d4601ab0182493ee7ddc5cf
nirajpatil2005 <nirajzpatil123@gmail.com>
2024-03-28 20:23:27 +0530

    collect

---
+++ Queues/Queue I/practice/removeEvenIndi.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+void display(queue<int> &q)
+{
+    int n = q.size();
+    for (int i = 0; i < n; i++)
+    {
+        int x = q.front();
+        q.pop();
+        cout << x << " ";
+        q.push(x);
+    }
+    cout << endl;
+}
+void removeEven(queue<int> q)
+{   
+    cout<<"after remove :";
+    int n = q.size();
+    for (int i = 0; i < n; i++)
+    {
+        int x = q.front();
+        q.pop();
+        if(i%2!=0) cout << x << " ";
+        if(i%2!=0)q.push(x);
+    }
+    cout << endl;
+}
+int main()
+{
+    queue<int> q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+    display(q);
+    removeEven(q);
+    q.push(100);
+    display(q);
+    return 0;
+}
