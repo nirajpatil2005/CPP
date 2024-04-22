5f86debb3dd9174ff06321a7092ec20d59e9db22
nirajpatil2005 <nirajzpatil123@gmail.com>
2024-04-22 23:39:48 +0530

    advance sorting

--- Advance Sorting/Merge sort/MergeSortI.cpp
+++ Advance Sorting/Merge sort/MergeSortI.cpp
@@ -24,9 +24,26 @@ void merge(vector<int>&a, vector<int>&b,vector<int>&res)
     if(j==b.size()) while(i<a.size())res[k++]=a[i++];
 
 }
-void mergeSort(vector<int>&a)
+void mergeSort(vector<int>&v)
 {
-
+    int n=v.size();
+    if(n==1)return;
+    int n1=n/2;
+    int n2=n-n/2;
+    vector<int>a(n1),b(n2);
+    for(int i=0;i<n1;i++)
+    {
+        a[i]=v[i];
+    }
+    for(int i=0;i<n2;i++)
+    {
+        b[i]=v[i+n1];
+    }
+    //magic aka recursion
+    mergeSort(a);
+    mergeSort(b);
+    //merge
+    merge(a,b,v);
 }
 int main()
 {
