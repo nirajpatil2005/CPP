55ff8694524e58341a6805cb3fcc5bfa65942bfa
nirajpatil2005 <nirajzpatil123@gmail.com>
2024-04-25 18:19:03 +0530

    new Binary Search

--- Searching/binarySearch.cpp
+++ Searching/binarySearch.cpp
@@ -1,25 +1,27 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-  class Solution {
+
+class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        //nums = [-1,0,3,5,9,12], target = 9
-        int lo=0;
-        int hi=nums.size()-1;
-        while(lo<=hi)
-        {
-            int mid=(hi+lo)/2;
-            if(nums[mid]==target) return true;
-            else if(nums[mid]>target) hi=mid-1;
-            else lo=mid+1;
-             }
-           
+    bool search(vector<int>& nums, int target) {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) return true;
+            else if (nums[mid] > target) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return false; // If target is not found
     }
-} ;
-int main()
-{
+};
+
+int main() {
     Solution sy;
-  vector<int> v={-1,0,3,5,9,12};
-  sy.search(v,0);
+    vector<int> v = {-1, 0, 3, 5, 9, 12};
+    bool found = sy.search(v, 0);
+    if (found) cout << "Target found\n";
+    else cout << "Target not found\n";
+    return 0;
 }
